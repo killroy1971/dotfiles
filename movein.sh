d6c9f9c64efaf132c4ba6d1106e02679a629a945
glenn <glennsnead@gmail.com>
2016-06-26 09:03:48 -0400

    Added script to set up shell configration, replacing the existing shell configuration.  Note: Does not seem to work on Cygwin. 	new file:   movein.sh

---
+++ movein.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script will remove existing bash shell config files, git config files, and vim config files, and replace them with symbolic links to this directory.
+# Glenn H. Snead
+# 26 Jun 2016
+
+if [ -d ${HOME}/.vim ]; then /bin/rm -Rf ${HOME}/.vim; fi
+if [ -f ${HOME}/.vimrc ]; then /bin/rm -f ${HOME}/.vimrc; fi
+if [ -f ${HOME}/.gvimrc ]; then /bin/rm -f ${HOME}/.gvimrc; fi
+if [ -f ${HOME}/.bash_profile ]; then /bin/rm -f ${HOME}/.bash_profile; fi
+if [ -f ${HOME}/.bashrc ]; then /bin/rm -f ${HOME}/.bashrc; fi
+if [ -f ${HOME}/.gitconfig ]; then /bin/rm -f ${HOME}/.gitconfig; fi
+if [ -f ${HOME}/.gitignore ]; then /bin/rm -f ${HOME}/.gitignore; fi
+
+ln -s ${HOME}/dotfiles/vim ${HOME}/.vim
+ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+ln -s ${HOME}/dotfiles/.gvimrc ${HOME}/.gvimrc
+ln -s ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
+ln -s ${HOME}/dotfiles/.bash_profile ${HOME}/.bash_profile
+ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
+ln -s ${HOME}/dotfiles/.gitignore ${HOME}/.gitignore
+
